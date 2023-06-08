4c516efe8ff3121b2e617892b735c7a32afa65bc
Pere Arbos <pedroarbos@hotmail.com>
2023-06-08 11:39:41 +0200

    added comments

--- DamavisChallenge.py
+++ DamavisChallenge.py
@@ -14,25 +14,31 @@ testNumber = input('Enter acceptance test number from 1 to 4 (you can edit them 
 
 labyrinth = tests[int(testNumber) -1]
 
+# rod position is stored as the orientation and which cell the head is in
 initialPosition = {'orientation': 'horizontal', 'head': [0, 2]}
 goal = [len(labyrinth) - 1, len(labyrinth[0]) - 1]
 
+# history of rod positions to avoid moving in circles
 visitedPositions = {'horizontal': [[0, 2]], 'vertical': []}
+# all paths for the rod that could potentially get to the goal
 currentPaths = [initialPosition]
 
 numberOfMoves = 0
 finish = False
 
+# loop finishes if goal is reached or ther is no path left that can reach it
 while (not finish):
     numberOfMoves += 1
     newPaths = []
     
     for path in currentPaths:
+        # calculate all new possible positiotns for every path
         newPositions = getPositionsAfterMoving(labyrinth, path['orientation'], path['head'])
         for position in newPositions:
             if position['head'] == goal:
                 finish = True
             
+            # avoid moving in circles
             if position['head'] in visitedPositions[position['orientation']]:
                 continue
             
--- functions.py
+++ functions.py
@@ -5,6 +5,7 @@ Created on Thu Jun  8 10:29:15 2023
 @author: pere
 """
 
+# checks if a cell is not blocked or out of bounds
 def isCellLegal(labyrinth, cell):
     if (cell[0] < 0 or cell[1] < 0):
         return False
@@ -12,9 +13,9 @@ def isCellLegal(labyrinth, cell):
         return False
     return labyrinth[cell[0]][cell[1]] == '.'
 
-
+# checks if all the cells the rod would occupy are legal
 def isPositionLegal(labyrinth, orientation, head):
-    rodCells = [head, [head[0], head[1] -1], [head[0], head[1] -2]]
+    rodCells = [head, [head[0], head[1] -1], [head[0], head[1] -2]] # rod cells if orientation is horizontal
     if orientation == 'vertical':
         rodCells = [head, [head[0] -1, head[1]], [head[0] -2, head[1]]]
         
@@ -24,9 +25,9 @@ def isPositionLegal(labyrinth, orientation, head):
                    
     return True
 
-
-def canChangeOrientation(labyrinth, orientation, head):
-    center = [head[0], head[1] -1]
+# checks if all the cells in the 3x3 area around the center are legal
+def canChangeOrientation(labyrinth, orientation, head): 
+    center = [head[0], head[1] -1] # center of the rod if orientations is horizontal
     if orientation == 'vertical':
         center = [head[0] -1, head[1]]
         
@@ -37,26 +38,26 @@ def canChangeOrientation(labyrinth, orientation, head):
             
     return True
 
-
+# gets the position of the rod after an orientation change
 def getChangeOrientation(orientation, head):
     if orientation =='horizontal':
         return {'orientation': 'vertical', 'head': [head[0] + 1, head[1] - 1]}
     
     return {'orientation': 'horizontal', 'head': [head[0] - 1, head[1] + 1]}
 
-
+# gets all legal positions after moving the rod up, down, left or right for one cell
 def getPositionsAfterMoving(labyrinth, orientation, head):
     newHeads = [[head[0] - 1, head[1]],
             [head[0] + 1, head[1]],
             [head[0], head[1] - 1],
-            [head[0], head[1] + 1]]
+            [head[0], head[1] + 1]] # possible cells the head would be after moving
     
     positions = []
-    for newHead in newHeads:
+    for newHead in newHeads: # filter moves that are not legal
         if (isPositionLegal(labyrinth, orientation, newHead)):
             positions.append({'orientation': orientation, 'head': newHead})
     
-    if canChangeOrientation(labyrinth, orientation, head):
+    if canChangeOrientation(labyrinth, orientation, head): # add change orientation if it is legal
         positions.append(getChangeOrientation(orientation, head))
         
     return positions
